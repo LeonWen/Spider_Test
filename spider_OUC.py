d7377625e4d2031f80674cba93963d34c7094bbd
Leon <wenhecpp@126.com>
2016-08-03 02:18:36 +0800

    添加糗事百科和海大积点查询

--- spider_OUC.py
+++ spider_OUC.py
@@ -2,8 +2,72 @@
 import urllib
 import urllib2
 import re
+import cookielib
+from bs4 import BeautifulSoup
 
-url = 'http://graduate.ouc.edu.cn/'
-request = urllib2.Request(url)
-response = urllib2.urlopen(request)
-print response.read()
+class OUC:
+    def __init__(self,username,password):
+        self.loginUrl = "http://graduate.ouc.edu.cn/j_acegi_security_check"
+        # self.leftFrameUrl = "http://graduate.ouc.edu.cn/listLeft.do?"
+        self.frameGradeUrl = "http://graduate.ouc.edu.cn/accessModule.do?moduleId=25011&amp;groupId="
+        # self.gradeUrl = "http://graduate.ouc.edu.cn/listMyBulletin.do?"
+        self.cookies = cookielib.CookieJar()
+        self.postdata = urllib.urlencode({
+            'j_username':username,
+            'j_password':password,
+            'groupId':''
+        })
+        self.opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookies))
+
+    def getPage(self):
+        user_agent = 'Mozilla/4.0(compatible;MSIE 5.5;Windows NT)'
+        headers = {'User-Agent':user_agent}
+        request = urllib2.Request(
+            url=self.loginUrl,
+            data=self.postdata,
+            headers=headers
+        )
+        result = self.opener.open(request)
+        gradeResult = self.opener.open(self.frameGradeUrl)
+        btfsp = BeautifulSoup(gradeResult.read())
+        # print  btfsp.prettify()
+
+        content = btfsp.body.find_all("tr")
+        resultTable = [[0 for col in range(3)] for row in range(14)]
+        print u'专业名称'.rjust(15),'\t',u'成绩','\t',u'学分'
+        i = 0
+        for item in content[1:]:
+            itemList = item.find_all("td")
+            classname = itemList[1].text.split(' ')[0].strip()
+            score = itemList[6].text.split('\n')[7].strip()
+            weight = itemList[7].text
+            resultTable[i][0] = classname
+            resultTable[i][1] = float(score)
+            resultTable[i][2] = float(weight)
+            print classname.rjust(15),'\t',score.rjust(3),'\t',weight.rjust(3)
+            i += 1
+            # print '%10s' % classname,'%-3s' % score.split('\n')[7],'\t',weight
+        # 计算GPA
+        sumGPA = 0
+        sumWeight = 0
+        for k in range(14):
+            sumWeight += resultTable[k][2]
+            sumGPA += resultTable[k][1] * resultTable[k][2]
+            gpa = sumGPA / sumWeight
+
+        print u'\n学分绩点为:',gpa
+
+
+    # def writeInfo(self,text):
+    #     for items in text:
+    #         pass
+
+if __name__ == '__main__':
+    # ouc_stu = OUC()
+    print '================ Welcome ===================='
+    print u'Please enter your login messages:'
+    username = input('username:')
+    password = input('password:')
+    print u'Waiting...'
+    ouc_stu = OUC(username,password)
+    ouc_stu.getPage()
--- spider_QSBK.py
+++ spider_QSBK.py
@@ -1,7 +1,13 @@
 # -*- coding:utf-8 -*-
+
+'''
+Author:LeonWen
+'''
+# 实现保存到文本的功能，并进行格式化输出
 import urllib
 import urllib2
-import re
+# import re
+from bs4 import BeautifulSoup
 
 page = 1
 url = 'http://www.qiushibaike.com/hot/page/' + str(page)
@@ -11,23 +17,65 @@ headers = {'User-Agent':user_agent}
 try:
     request = urllib2.Request(url,headers=headers)
     response = urllib2.urlopen(request)
-
-    # content = response.read().decode('utf-8')
-    # pattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class'+
-    #                      '="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>',re.S)
-    # items = re.findall(pattern,content)
-    # for item in items:
-    #     print item[0],item[1],item[2],item[3],item[4]
-
-    content = response.read().decode('utf-8')
-    # '<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>'
-    # authorPattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?title=(.*?)>.*?</a>')
-    authorPattern = re.compile('<div.*?class="content".*?title="(.*?)">(.*?)</div>',re.S)
-    author = re.findall(authorPattern,content)
-    # 这里的正则匹配一直搞不明白啊
+    object_bs = BeautifulSoup(response.read())
+    # print object_bs.prettify()
+    # items 是一个list保存着返回结果
+    items = object_bs.body.find_all("div",{"class":"article block untagged mb15"})
+    # print items
+    floor = 1
+    tag = 0
+    for item in items:
+        if item.find("div",{"class":"thumb"}) == None:
+            # class=thumb为带有图片的标签
+            author = item.find("h2")
+            upNum = item.find("i",{"class":"number"})
+            content = item.find("div",{"class":"content"})
+            # print content.prettify()
+            # print content.text
+            print u"===============",floor,u" 楼 ======================="
+            print u"作者:",author.text
+            print u"赞同数:",upNum.text
+            print u"内容:",content.get_text()
+            floor += 1
+        else:
+            tag += 1
+    print u"图片个数:",tag
 except urllib2.URLError,e:
-    # hasattr means if has the attribution
     if hasattr(e,"code"):
         print e.code
     if hasattr(e,"reason"):
         print e.reason
+    # content = response.read().decode('utf-8')
+    # pattern = re.compile('.*?(.*?).*?(.*?)',re.S)
+    # items = re.findall(pattern,content)
+    # for item in items:
+    #     print item[0],item[1],item[2],item[3],item[4]
+    #
+    # pageContent = response.read().decode('utf-8')
+    # # '<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?>(.*?)</a>.*?<div.*?class="content".*?title="(.*?)">(.*?)</div>(.*?)<div class="stats.*?class="number">(.*?)</i>'
+    # # authorPattern = re.compile('<div.*?class="author.*?>.*?<a.*?</a>.*?<a.*?title=(.*?)>.*?</a>')
+    # authorPattern = re.compile('<atitle="(.*?)>"',re.S)
+    # authorLists = re.findall(authorPattern,pageContent)
+    # print authorLists
+    #
+    # timePattern = re.compile('',re.S)
+    # timeLists = re.findall(timePattern,pageContent)
+    #
+    # upPattern = re.compile('',re.S)
+    # upLists = re.findall(upPattern,pageContent)
+    #
+    # contentPattern = re.compile('<div.*?class="content">(.*?)</div>',re.S)
+    # contentlists = re.findall(contentPattern,pageContent)
+    # floor = 1
+    # for author in authorlists:
+    #     print floor,u'楼=========================='
+    #     print author
+    #     floor += 1
+    # # print author[0]
+
+# except urllib2.URLError,e:
+#     # hasattr means if has the attribution
+#     if hasattr(e,"code"):
+#         print e.code
+#     if hasattr(e,"reason"):
+#         print e.reason
